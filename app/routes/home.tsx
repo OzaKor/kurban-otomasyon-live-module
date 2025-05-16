1e220762f13679d4c34533f19f6b02b9f42a3c8c
HakanKorkz <hakan.hizmet.54.gs@gmail.com>
2025-05-16 18:08:31 +0300

    feat: Refactor home route import and export
    Refactor the home route to use a relative import for the Welcome
    component and move the default export to the end of the file.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,5 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import {Welcome} from "~/welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -8,6 +8,8 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export default function Home() {
+function Home() {
   return <Welcome />;
 }
+
+export default Home;
