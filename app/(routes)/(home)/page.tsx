458f4f3dc91737ff868ba523c75d71ebb57a9589
HakanKorkz <hakan.hizmet.54.gs@gmail.com>
2025-05-19 22:09:06 +0300

    feat(breadcrumb): add breadcrumb component
    The changes in this commit introduce a new breadcrumb component to the application. The breadcrumb component provides a way to display the user's current location within the application's hierarchy.
    
    The main changes include:
    
    1. Create a new `components/ui/breadcrumb.tsx` file that defines the various components that make up the breadcrumb, such as `Breadcrumb`, `BreadcrumbList`, `BreadcrumbItem`, `BreadcrumbLink`, `BreadcrumbPage`, `BreadcrumbSeparator`, and `BreadcrumbEllipsis`.
    
    2. Integrate the breadcrumb component into the `app/(routes)/(home)/page.tsx` file, where it is used to display the user's current location on the home page.
    
    3. Add a new `components/breadcrumbs.tsx` file that provides a higher-level wrapper around the breadcrumb components, making it easier to use in the application.
    
    These changes improve the user experience by providing a clear visual representation of the user's location within the application, making it easier for them to navigate and understand the application's structure.

--- app/(routes)/(home)/page.tsx
+++ app/(routes)/(home)/page.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React from "react";
+import Breadcrumbs from "@/components/breadcrumbs";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import Main from "./_components/main";
 
 const Home = () => {
-    return (
-        <div>
-            Kesim Listesi gelecek
-        </div>
-    );
+  return (
+    <>
+      <Breadcrumbs items={[
+        {
+            href: "/",
+            label: "Anasayfa",
+        },
+        {
+            href: null,
+            label: "Kesim Listesi",
+        },
+      ]} />
+      <Card className="py-2">
+        <CardHeader className="p-0">
+          <CardTitle className="text-xl font-bold px-6 text-gray-800">Kesim Listesi</CardTitle>
+          <Separator />
+        </CardHeader>
+        <CardContent className="p-6">
+          <Main />
+        </CardContent>
+      </Card>
+    </>
+  );
 };
 
 export default Home;
