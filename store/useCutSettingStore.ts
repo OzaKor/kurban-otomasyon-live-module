54ac0e5d71558466a07ba2fc4efab80616429d51
HakanKorkz <hakan.hizmet.54.gs@gmail.com>
2025-05-20 17:49:16 +0300

    feat: Implement user authentication and authorization
    Adds a new user authentication and authorization system to the
    application. Users can now register, log in, and manage their
    account settings. Administrators can also manage user roles and
    permissions. This feature is crucial for securing the application
    and providing a personalized experience for users.

--- store/useCutSettingStore.ts
+++ store/useCutSettingStore.ts
@@ -3,48 +3,43 @@ import axios from "@/lib/axios";
 import { useUserStore } from "./userStore";
 
 export type CutSettingStoreType = {
-   proccessStart:boolean,
-   proccessEnd:boolean,
-   processContiune:boolean,
-   processStop:boolean    
-}
+  proccessStart: boolean;
+  proccessEnd: boolean;
+  processContiune: boolean;
+  processStop: boolean;
+};
 interface CutSettingStore {
-    state:CutSettingStoreType,
-    setState:(state:CutSettingStoreType)=>void,
-    fetchCutSetting:()=>void,
-    clear:()=>void
+  state: CutSettingStoreType;
+  setState: (state: CutSettingStoreType) => void;
+  fetchCutSetting: () => Promise<void>;
+  clear: () => void;
 }
 
 const initialState: CutSettingStoreType = {
-    proccessStart: false,
-    proccessEnd: false,
-    processContiune: false,
-    processStop: false
+  proccessStart: false,
+  proccessEnd: false,
+  processContiune: false,
+  processStop: false,
 };
 
-export const useCutSettingStore = create<CutSettingStore>()(
-    (set) => ({
-        state: initialState,
-        setState:(state:CutSettingStoreType)=>set({state}),
-        fetchCutSetting:()=>{
-            const {userToken} = useUserStore.getState();
-            if(!userToken){
-                return;
-            }
-            axios.get("/api/cut-setting",{
-                headers:{
-                    Authorization:`Bearer ${userToken}`,
-                    "Content-Type":"application/json",
-                    "Accept":"application/json"
-                }
-            })
-            .then((response)=>{
-                set({state:response.data})
-            })
-            .catch((error)=>{
-                console.log(error)
-            })
-        },
-        clear: () => set({ state: initialState })
-    })
-)
+export const useCutSettingStore = create<CutSettingStore>()((set) => ({
+  state: initialState,
+  setState: (state: CutSettingStoreType) => set({ state }),
+  fetchCutSetting: async () => {
+    const { userToken } = useUserStore.getState();
+    if (!userToken) {
+      return;
+    }
+    const response = await axios.get("/api/cut-settings");
+    console.log("setting response: ", response);
+    set({
+      state: {
+        proccessStart: response.data.proccessStart,
+        proccessEnd: response.data.proccessEnd,
+        processContiune: response.data.processContiune,
+        processStop: response.data.processStop,
+      },
+    });
+  },
+  clear: () => set({ state: initialState }),
+}));
